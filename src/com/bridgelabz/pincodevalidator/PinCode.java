e1f96e1034487d65fa430b69b8a7b6669233e094
Akash Singh <akashsingh6280@gmail.com>
2021-09-08 01:45:33 +0530

    ADDED: using regex to check 6 digit pincode

---
+++ src/com/bridgelabz/pincodevalidator/PinCode.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.pincodevalidator;
+import java.util.regex.*;  
+public class PinCode {
+
+	public static void validatePin(String pincode) {
+		
+		boolean result = Pattern.matches("[0-9]{6}", pincode);
+		if(result) {
+			System.out.println("Valid");
+		}
+		else {
+			System.out.println("Invalid");
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		validatePin("400088");
+	}
+	
+	
+}
