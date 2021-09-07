1f1c7b5763218e5c05bfc2a8509b84cfd0277ead
Akash Singh <akashsingh6280@gmail.com>
2021-09-08 02:15:19 +0530

    MODIFIED: pincode with space must be accepted

--- src/com/bridgelabz/pincodevalidator/PinCode.java
+++ src/com/bridgelabz/pincodevalidator/PinCode.java
@@ -4,7 +4,7 @@ public class PinCode {
 
 	public static void validatePin(String pincode) {
 		
-		boolean result = Pattern.matches("[0-9]{6}$", pincode);
+		boolean result = Pattern.matches("^[0-9]{3}[ ][0-9]{3}$", pincode);
 		if(result) {
 			System.out.println("Valid");
 		}
@@ -15,7 +15,7 @@ public class PinCode {
 
 	public static void main(String[] args) {
 		
-		validatePin("400088B");
+		validatePin("400   088");
 		
 	}
 	
